1a2d506dbab0caa2f61cc888ea26798fc22aa868
Amol Pandurang Late <Amol.Late@ad.infosys.com>
2018-04-13 18:39:54 +0530

    added SSL

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-
+var fs = require("fs");
 var routes = require('./api/routes/index');
+var https = require("https");
 
 // Define the port to run on
 app.set('port', 5000);
@@ -24,6 +25,24 @@ app.use(bodyParser.json());
 // Add some routing
 app.use('/api', routes);
 
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
+app.get("/", function(req, res){
+
+  res.send("<h1>hello<h2>");
+  
   });
+  
+  var options = {
+  
+  key: fs.readFileSync("insteadq.key"),
+  
+  cert: fs.readFileSync("insteadq.cert")
+  
+  }
+  
+var server = https.createServer(options, app);
+console.log("Listen to 5000");
+server.listen(5000);
+
+// Listen for requests
+//var server = app.listen(app.get('port'), function() {
+ // });
