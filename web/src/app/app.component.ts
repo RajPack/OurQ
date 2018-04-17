06f76c1c537ee930624e1c65726ed84b52fea3ce
Chethan Srinivas Rao <csindhe@gmail.com>
2018-04-17 12:48:53 +0530

    Eleventh Commit

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showMinimalSearch: boolean = false;
+  showMiniBar(){
+    console.log(this.isInViewport("topbar"));
+    this.showMinimalSearch= this.isInViewport("topbar") ? false: true;
+  }
+
+
+  isInViewport(args) {
+    var elem = document.getElementById('topbar');
+    var elementTop = elem.clientTop;
+    var elementBottom = elementTop + elem.clientHeight;
+    var viewportTop = window.pageYOffset;
+    var viewportBottom = viewportTop + window.innerHeight	;
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
 }
--- web/src/app/topbar/topbar.component.ts
+++ web/src/app/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
     selector: 'app-topbar',
@@ -7,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class TopBarComponent implements OnInit {
-
+    @Input() showMinimalSearch: boolean;
     constructor() {
 
     }
