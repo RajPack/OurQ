6246a364954be89f558c8430747e3641532bdf8f
rajpack <letsthinkbetter@gmail.com>
2018-04-27 11:01:19 +0530

    updated rbuilder

--- q-app/src/data/data.js
+++ q-app/src/data/data.js
@@ -11,7 +11,7 @@ export const data = {
         {title: "BackBone JS", rating: 4}
     ],
     secondarySkills: [
-        "HTML, CSS, Sass, Jquery", "Javascript, Typescript", 
+        "HTML, CSS, Sass, Jquery", "Javascript, Typescript, Angular + Nativescript", 
         "Angular (2+), Angular JS, Angular + Redux", 
         "Gulp, Webpack", 
         "Nodejs", "Responsive Web Design" 
@@ -19,15 +19,15 @@ export const data = {
     education : "B.E. Computer Science",
     workExperience : [
         {
-            role: "Front end UI Programmer", company:"Infosys Ltd.", period: "Oct 2017 - Present",
+            role: "Front end UI Programmer", company:"Infosys Ltd.", period: "Oct 2017 - Present", logo: "../assets/images/infosys.png",
             synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
         },
         {
-            role: "Technical Lead / Programmer", company:"Tech Mahindra Ltd.", period: "Oct 2017 - Present",
+            role: "Technical Lead / Programmer", company:"Tech Mahindra Ltd.", period: "Oct 2017 - Present", logo: "../assets/images/techm.png",
             synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
         },
         {
-            role: "Team Lead", company:"Estuate Pvt. Ltd.", period: "Oct 2017 - Present",
+            role: "Team Lead", company:"Estuate Pvt. Ltd.", period: "Oct 2017 - Present", logo: "../assets/images/estuate.jpg",
             synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
         }
     ]
--- q-app/src/mainContent/maincontent.js
+++ q-app/src/mainContent/maincontent.js
@@ -16,6 +16,7 @@ export default class MainContent extends React.Component {
             const item = 
            ( <div className="work-exp-item" key={index}>
                 <div className="job-title">{work.role}</div>
+                <div className="logo-container"><img src={work.logo}></img></div>
                 <div className="company-title">{work.company}</div>
                 <div className="period">{work.period}</div>
                 <div className="work-content">{work.synopsis}</div>
