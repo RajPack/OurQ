41065273d6c598e05883954e4f9c3fbd1a99fd59
rajpack <letsthinkbetter@gmail.com>
2018-05-02 11:03:08 +0530

    upated

--- q-app/src/data/data.js
+++ q-app/src/data/data.js
@@ -1,5 +1,9 @@
 export const data = {
-    about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    about: `I am a strong front end web programmer with excellent javascript skills. 
+            
+        My current forte is Angular (2+) and exprienced in numerous projects involving various front end frameworks such as Rxjs, Redux, React JS, Backbone JS.
+        
+        Also proficient in Javascript Design Patterns and pay utmost importance to the architectural decisions and design patterns applied to each and every part of the application code.`,
     primarySkills: [
         {title: "Angular", rating: 4.5},
         {title: "React JS", rating: 3.5},
@@ -14,22 +18,32 @@ export const data = {
         "HTML, CSS, Sass, Jquery", "Javascript, Typescript, Angular + Nativescript", 
         "Angular (2+), Angular JS, Angular + Redux", 
         "Gulp, Webpack", 
-        "Nodejs", "Responsive Web Design" 
+        "Nodejs + Express", "Responsive Web Design" 
     ],
     education : "B.E. Computer Science",
     totalExp: "7 Years, 4 Months",
     workExperience : [
         {
             role: "Front end UI Programmer / Consultant", company:"Infosys Ltd.", period: "Oct 2017 - Present", logo: "../assets/images/infosys.png",
-            synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            synopsis: `Developed applications using Angular, RXJS and Redux. Designed and developed custom view-model* and Observables frameworks for an asset management application for Dell that encouraged in-house frameworks over other frameworks such as angular or React.
+            
+            Developed a cafeteria application to order food using Angular + Nativescript (android and Web) as side project during spare time.
+            `
         },
         {
             role: "Technical Lead / Programmer", company:"Tech Mahindra Ltd.", period: "Mar 2015 - Oct 2017", logo: "../assets/images/techm.png",
-            synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            synopsis: `Designed and developed applications in React JS and Angular JS (1.x).
+                        
+            Built and enhanced order management application using React Js.
+            Developed an Service management and tracking application for a telecom company using Angular JS 1.x.
+            Involved in couple of POCs with primary focus on Javascript and responsive web layouts.`
         },
         {
             role: "Team Lead", company:"Estuate Pvt. Ltd.", period: "Jan 2011 - Mar 2015", logo: "../assets/images/estuate.jpg",
-            synopsis: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+            synopsis: `Built various web applications using Javascript, Backbone JS, HTML, CSS, Jquery while paying importance to Design Patterns.
+
+
+            `
         }
     ]
 }
